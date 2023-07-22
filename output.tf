fbcc623e648a83eef61544b2ef461dd18cd1c591
sarathsr785 <kkumar.htaras@gmail.com>
2023-07-22 18:24:38 +0530

    Initial commit: Adding code to the repository

---
+++ output.tf
@@ -0,0 +1,16 @@
+output "vpc_id" {
+  value       = module.vpc.vpc_id
+  description = "VPC ID"
+}
+output "public_subnets" {
+  value       = module.vpc.public_subnets
+  description = "VPC public subnets' IDs list"
+}
+output "private_subnets" {
+  value       = module.vpc.private_subnets
+  description = "VPC private subnets' IDs list"
+}
+output "cluster_id" {
+  value       = module.eks.cluster_name
+  description = "EKS Cluster Name ID"
+}
